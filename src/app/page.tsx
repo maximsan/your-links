121c35ae8954eb54f30426dee6f4f781b08c828d
maksimshchutski <maksim.shchutski@itrexgroup.com>
2023-07-16 00:40:33 +0300

    XXX: add icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { YourLink } from '@/components/YourLink';
 
 export default function Home() {
   return (
-    <main className="container mx-auto max-w-5xl my-20">
+    <main className="container mx-auto max-w-5xl my-8">
       <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {links.map((link) => (
           <YourLink
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { Link as LinkIcon } from 'react-feather';
 
 export const Header = () => {
   return (
@@ -6,22 +7,9 @@ export const Header = () => {
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <Link
           href="/"
-          className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+          className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0  border rounded-full bg-blue-600 border-blue-800 p-2.5"
         >
-          <svg
-            className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
-            ></path>
-          </svg>
+          <LinkIcon className="text-white" />
         </Link>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
           {/*{user ? (*/}
@@ -46,7 +34,7 @@ export const Header = () => {
           {/*  </div>*/}
           {/*) : (*/}
           <Link
-            href="/api/auth/login"
+            href="/"
             className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
           >
             Login
